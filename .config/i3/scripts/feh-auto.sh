ec6041458c8c537c5d8ff9c00bf2aa54d06a8edc
Shifat <hakim.shifat@gmail.com>
2024-12-07 23:00:25 +0600

    fixed

--- .config/i3/scripts/feh-auto.sh
+++ .config/i3/scripts/feh-auto.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do
   feh ~/Pictures/Wall --randomize --bg-fill
- # feh ~/Pictures/Wall/Familymode --randomize --bg-fill
+#  feh ~/Pictures/Wall/Familymode --randomize --bg-fill
   sleep 1200
 done
